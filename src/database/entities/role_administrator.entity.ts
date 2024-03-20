c20910a721aa285ff9c9208540905e8a6d899196
Katerine Morcillo <kmorcillo312@hotmail.com>
2024-03-20 09:50:58 -0500

    Se agregó tabla rol del administrador

--- src/database/entities/role_administrator.entity.ts
+++ src/database/entities/role_administrator.entity.ts
@@ -1,4 +1,37 @@
-import { Entity } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Status } from "./enums/status.enum";
 
 @Entity()
-export class Role_administraror {}
+export class Role_administraror {
+  @PrimaryGeneratedColumn("uuid", {
+    comment: "Llave primaria rol del adimistrador",
+  })
+  id: string;
+
+  @Column({ comment: "Nombre del rol del administrador" })
+  name: string;
+
+  @Column({
+    type: "enum",
+    enum: Status,
+    default: Status,
+    comment: "Estado del rol del administrador:activo o inactivo",
+  })
+  status: string;
+
+  @CreateDateColumn({ comment: "Fecha e creación del rol" })
+  createAt: Date;
+
+  @UpdateDateColumn({ comment: "Fecha de actualización del rol" })
+  updateAt: Date;
+
+  @DeleteDateColumn({ comment: "Fecha de eliminación del rol" })
+  deleteAt: Date;
+}
